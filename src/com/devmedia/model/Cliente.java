f3fc0db7a2687c3088f1ad6469e3bd02f0a5ab6c
wendelmenegasso <wendelpereiramene@gmail.com>
2022-06-17 19:26:59 -0300

    Adicionando a classe Cliente

---
+++ src/com/devmedia/model/Cliente.java
@@ -0,0 +1,28 @@
+package com.devmedia.model;
+
+import com.devmedia.negocio.account.AccountImpl;
+import com.devmedia.negocio.extract.ExtractImpl;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Cliente {
+
+    private AccountImpl account;
+    private ExtractImpl extract;
+
+    public ExtractImpl getExtract() {
+        return extract;
+    }
+
+    public void setExtract(ExtractImpl extract) {
+        this.extract = extract;
+    }
+
+    public AccountImpl getAccount() {
+        return account;
+    }
+
+    public void setAccount(AccountImpl account) {
+        this.account = account;
+    }
+}
